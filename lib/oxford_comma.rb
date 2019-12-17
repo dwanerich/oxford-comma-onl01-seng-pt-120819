01ff8877469a68101546bd32b0d6520a16d18c0d
dwane richards <dwanerich@gmail.com>
2019-12-17 02:06:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,14 @@
 def oxford_comma(array)
- array.join.to_s
 
- if array.length == 1
-   array.first
- elsif array.length == 2
-   array.join(" and ")
- elsif array.length > 2
-   part_1 = array[0..-2]
-   part_2 = [[array.last].unshift("and ").join]
-   combined = part_1 + part_2
-   combined.join(", ")
-   
- end
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    part_1 = array[0..-2]
+    part_2 = [[array.last].unshift("and ").join]
+    combined = part_1 + part_2
+    combined.join(", ")
+  end
 
 end
